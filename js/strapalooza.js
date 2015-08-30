817289baa823281bb225b819f96620566ef10d75
Paul Sauve <paul@burngames.net>
2015-08-30 00:44:57 -0500

    Redid buttons

--- js/strapalooza.js
+++ js/strapalooza.js
@@ -17,4 +17,34 @@
             $('.in[data-target="' + id + '"]').toggleClass('in');
         }
     });
+
+    var $div;
+
+    $('.btn-ripple').on('mousedown', function (event) {
+        if ($div) {
+            $div.remove();
+        }
+
+        $div = $('<div/>');
+        var btnOffset = $(this).offset();
+        var xPos = event.pageX - btnOffset.left;
+        var yPos = event.pageY - btnOffset.top;
+
+        $div.addClass('ripple-effect');
+        var $ripple = $(".ripple-effect");
+
+        $ripple.css("height", 4);
+        $ripple.css("width", 4);
+        $div.css({
+            top: yPos - 2.5,
+            left: xPos - 2.5,
+            background: $(this).hasClass('btn-link') ? '#222222' : '#ffffff'
+        }).appendTo($(this));
+    });
+
+    $(document).on('mouseup', function () {
+        if ($div) {
+            $div.addClass('removed');
+        }
+    });
 })(jQuery);
